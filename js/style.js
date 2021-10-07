e6c9f5ee0cd6baf9b04cf75c1f926e98c1112744
Mahabub-Alam-Bhuiyan <ma.nahid960@gmail.com>
2021-10-07 19:17:29 +0600

    third commit

---
+++ js/style.js
@@ -0,0 +1,22 @@
+// external js: flickity.pkgd.js
+
+// var flkty = new Flickity('.carousel-inner');
+// var carouselStatus = document.querySelector('.carousel-status');
+//
+// function updateStatus() {
+//     var slideNumber = flkty.selectedIndex + 1;
+//     carouselStatus.textContent = slideNumber + '/' + flkty.slides.length;
+// }
+// updateStatus();
+//
+// flkty.on( 'select', updateStatus );
+
+
+// let totalItems = $('.carousel-item').length;
+// let currentIndex = $('div.active').index() + 1;
+//
+// $('#carouselExampleIndicators').on('slid.bs.carousel', function() {
+//     currentIndex = $('div.active').index() + 1;
+//     $('.carousel-status').html(''+currentIndex+'/'+totalItems+'');
+// });
+
